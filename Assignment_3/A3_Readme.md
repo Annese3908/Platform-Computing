b427530fe021a4347a02c468a42eb1b511ded1b2
Annese3908 <008493908@coyote.csusb.edu>
2024-03-25 19:39:18 -0700

    Moving metric_tracker code from assignment 2 to assignment 3 and completing readme

--- Assignment_3/A3_Readme.md
+++ Assignment_3/A3_Readme.md
@@ -2,17 +2,17 @@
 
 PROVIDE OVERVIEW OF ASSIGNMENT HERE
 
-Time spent: **NUMBER_HOURS_SPENT** hours spent in total
+Time spent: 6-7 hours spent in total
 
 ## Features
 
 The following **required** features are completed:
 
-- [ ] Organize Github projects for Assignment 3 (2 pts)
-- [ ] Complete SQL Mystery Walkthrough and take a screenshot or print to PDF the webpage (2 pts)
-- [ ] Complete Interactive MongoDB Tutorial and take a screenshot or print to PDF the webpage  (2 pts)
-- [ ] Choose a SQL or NoSQL Database and set up a server (4 pts)
-- [ ] Modify your metric_tracker file so that the metrics are saved onto a database (5 pts)
+- [X] Organize Github projects for Assignment 3 (2 pts)
+- [X] Complete SQL Mystery Walkthrough and take a screenshot or print to PDF the webpage (2 pts)
+- [X] Complete Interactive MongoDB Tutorial and take a screenshot or print to PDF the webpage  (2 pts)
+- [X] Choose a SQL or NoSQL Database and set up a server (4 pts)
+- [X] Modify your metric_tracker file so that the metrics are saved onto a database (5 pts)
 
 The following **bonus** features are implemented:
 
@@ -22,8 +22,10 @@ The following **bonus** features are implemented:
 
 ## Screenshot and/or Video Walkthrough
 
-<img src="https://imgur.com/gallery/4rAXx5x" title='Example Video Walkthrough' width='' alt='Example Video Walkthrough' />
-
+<img src="Images/Screenshot 2024-03-23 at 4.37.54 PM.png" title='SQL Mystery Walkthrough Completion' width='' alt='SQL Mystery Walkthrough Completion' />
+<img src="Images/Screenshot 2024-03-23 at 4.45.12 PM.png" title='MongoDB Tutorial Completion' width='' alt='MongoDB Tutorial Completion' />
+<img src="Images/Screenshot 2024-03-25 at 7.33.31 PM.png" title='MySQL Database and Table' width='' alt='MySQL Database and Table' />
+<img src="Images/Screenshot 2024-03-25 at 7.19.51 PM.png" title='Metrics Table with Values' width='' alt='Metrics Table with Values' />
 
 ## Notes:
 PROVIDE RELEVANT OR ADDITIONAL INFORMATION HERE. Below are formatting options to add emphasis in text
--- Assignment_3/Metric_Tracker/metric_tracker.js
+++ Assignment_3/Metric_Tracker/metric_tracker.js
@@ -1,10 +1,22 @@
 const {By, Builder} = require('selenium-webdriver');
-const assert = require("assert");
-let metrics = [];
-// async function to periodically track changing values
-function writeToCSV(){
+const assert = require('assert');
 
-}
+const mysql = require('mysql2');
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Camann2003',
+    database: 'metrics_db',
+    port: 3306
+})
+con.connect(function(err){
+    if(err) throw err;
+    console.log('Connected!');
+})
+
+let metrics = {};
+// async function to periodically track changing values
+
  (async function trackMetrics(){
     let driver;
     try{
@@ -33,26 +45,64 @@ function writeToCSV(){
          });
          
          // variables to watch time on page
+         
          const start_time = Date.now();
-         let watch_time = start_time;
-         while(true){
+         let watched = start_time;
+        
+         // count for while loop execution cutoff
+         let count = 0;
+         while(count < 3){
+            let time = new Date().toISOString().slice(0, 19).replace('T', ' ');
+            // initialize metrics keys
+            metrics.timestamp = metrics.timestamp || [];
+            metrics.page_title = metrics.page_title || [];
+            metrics.watch_time = metrics.watch_time || [];
+            metrics.scrolled_pixels = metrics.scrolled_pixels || [];
+            // push pre-initialized metrics
+            metrics.timestamp.push(time);
+            metrics.page_title.push(title);
             // track time
             const curr_time = Date.now();
-            watch_time = curr_time - start_time;
-            console.log( {watch_time} ,' milliseconds');
+            watched = (curr_time - start_time)/1000;
+            console.log( {watched} ,' seconds');
+            // push watch time
+            metrics.watch_time.push(watched);
             // track scrolling
             const scroll_height = await driver.executeScript("return document.body.scrollHeight");
-            curr_scroll = await driver.executeScript("return window.pageYOffset");
+            let curr_scroll = await driver.executeScript("return window.pageYOffset");
             console.log('Scrolled', curr_scroll, '/', scroll_height, ' pixels');
-       
+            // push scrolled height
+            let scrolled = curr_scroll + '/' + scroll_height;
+            metrics.scrolled_pixels.push(scrolled)
+
             await driver.sleep(2000);
-        } 
+            count++;
+        }
+        // transform object into array of arrays based off of primary key
+        let data = [];
+
+        for (let i = 0; i < metrics.timestamp.length; i++) {
+        data.push([
+            metrics.timestamp[i],
+            metrics.page_title[i],
+            metrics.scrolled_pixels[i],
+            metrics.watch_time[i],
+        ]);
     }
+        // insert into table
+        const sql = 'INSERT INTO metrics (timestamp, page_title, scrolled_pixels, watch_time) VALUES ?'
+        con.query(sql, [data], (err, result) => {
+            if (err) throw err;
+            console.log('Number of records inserted:', result.affectedRows);
+          });     
+    }
+
     catch(e){
         console.log(e);
     }
     finally{
         await driver.quit();
     }
- 
+    console.log(metrics);
+    con.end;
  }())
